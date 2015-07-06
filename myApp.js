c6d35c82371de5f10c300f263b6207ab91682fdc
rindra <rahrindra@gmail.com>
2015-07-06 17:12:25 +0300

    ajout des menu à partir d'une directive

--- myApp.js
+++ myApp.js
@@ -1,5 +1,30 @@
 var myApp = angular.module('myApp', []);
 
+myApp.directive('menuAriary', function () {
+  return {
+    restrict: 'EA',
+    replace: true,
+    transclude: true,
+    templateUrl: 'templates/menu.html',
+    link: function (scope, element, attrs) {
+      // DOM manipulation/events here!
+    }
+  };
+});
+
+myApp.directive('menuUtilisateur', function () {
+  return {
+    restrict: 'EA',
+    replace: true,
+    transclude: true,
+    templateUrl: 'templates/menu-utilisateur.html',
+    link: function (scope, element, attrs) {
+      // DOM manipulation/events here!
+    }
+  };
+});
+
+
 myApp.controller('ageController', function($scope){
 	$scope.age = 0;
 	$scope.name = "testa";
